af94afab96b8df0062f66929b81ec5693eda7ce2
eifo1 <buttonmash61@gmail.com>
2023-05-03 19:17:10 -0400

    remove licenses from all files

--- config/site.php
+++ config/site.php
@@ -1,28 +1,5 @@
 <?php
-/**
- * MIT License
- *
- * Copyright (c) 2021-2022 FoxxoSnoot
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
+ 
 return [
     'name' => env('APP_NAME', 'Laravel Roblox Clone'),
     'logo' => '/img/logo.png',
@@ -33,7 +10,7 @@ return [
 
     'route_domains' => [
         'admin_site' => 'panel.domain.com',
-        'main_site' => 'www.domain.com'
+        'main_site' => 'localhost'
     ],
 
     'storage_url' => 'http://cdn.domain.com',
